3f28e388e5f5325e189b82d0eba10c484b541909
Darío Fernández <darioflo19@icloud.com>
2025-04-19 21:21:02 -0600

    aleatorio

--- src/CriaturasMarinas.java
+++ src/CriaturasMarinas.java
@@ -2,10 +2,12 @@ public abstract class CriaturasMarinas {
 
     String nombre;
     int tamaño;
+    String lugar;
 
     CriaturasMarinas(String nombre, int tamaño){
         this.nombre = nombre;
         this.tamaño = tamaño;
+        this.lugar = lugar;
     }
 
     abstract void nadar();
